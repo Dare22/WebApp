b313f4db95adb45d148baeed98f8267f3019f27b
Dare22 <daregm.dk@gmail.com>
2016-11-17 21:23:29 +0100

    Disable error handler controller

--- WebApp/src/controller/ErrorHandlerController.java
+++ WebApp/src/controller/ErrorHandlerController.java
@@ -4,20 +4,14 @@ import org.hibernate.HibernateException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
 public class ErrorHandlerController {
 
-	@ControllerAdvice
-	public class ErrorHandler {
-		
-		@ExceptionHandler(HibernateException.class)
-		public String DataBaseErrorHandler(HibernateException ex) {
-			//ex.printStackTrace();
-			return "error";
-
-		}
+/*	@ExceptionHandler(HibernateException.class)
+	public String DataBaseErrorHandler(HibernateException ex) {
+		// ex.printStackTrace();
+		return "error";
 
 	}
-
-	
-	
+*/
 }
